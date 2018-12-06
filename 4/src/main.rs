e978b63a8bd1145cc4a5ee868ba4e9f878aa58f8
Mikko Kaistinen <mikko.kaistinen@kapsi.fi>
2018-12-06 18:15:20 +0200

    4b done

--- 4/src/main.rs
+++ 4/src/main.rs
@@ -30,7 +30,7 @@ impl<'a> SleepTime<'a> {
     }
   }
 
-  pub fn process_sleep_times(&mut self, log: &'a Vec<Entry>) {
+  pub fn process_sleep_times(&mut self, log: &'a [Entry]) {
     let mut curr_quard = "";
     let mut is_awake = true;
     let mut bed_time = Utc::now();
@@ -55,15 +55,15 @@ impl<'a> SleepTime<'a> {
 
   pub fn get_sleepiest_elf(&self) -> (&str, u32) {
     let mut biggest = ("", 0);
-    for (key, val) in self.sleeps.iter() {
-      if val > &biggest.1 {
-        biggest = (key, *val)
+    for (key, &val) in self.sleeps.iter() {
+      if val > biggest.1 {
+        biggest = (key, val)
       }
     }
     biggest
   }
 
-  pub fn most_common_minute_to_sleep(&self, elf_id: &str, log: &Vec<Entry>) -> u32 {
+  pub fn most_common_minute_to_sleep(&self, elf_id: &str, log: &[Entry]) -> (u32, u32) {
     let mut sleep_times = vec![];
     let mut curr_quard = "";
     let mut is_awake = true;
@@ -77,7 +77,6 @@ impl<'a> SleepTime<'a> {
           bed_time = entry.date_time;
         } else {
           let mut times = calc_minute_range(&bed_time, &entry.date_time);
-          println!("{:?}", times);
           sleep_times.append(&mut times);
         }
       } else {
@@ -91,9 +90,19 @@ impl<'a> SleepTime<'a> {
         .or_insert(1);
     });
     match minutes.iter().max_by(|(_, &a), (_, &b)| a.cmp(&b)) {
-      Some(val) => *val.0,
+      Some(val) => (*val.0, *val.1),
       None => panic!("No value found")
     }
+  }
+
+  pub fn most_minute_to_sleep_by_one_elf(&self, log: &[Entry]) -> (&str, u32, u32) {
+    let all_ids = self.sleeps.iter().map(|(&id, _), | id).collect::<Vec<_>>();
+    all_ids.iter().map(|&id| {
+      let times = self.most_common_minute_to_sleep(id, log);
+      (id, times.0, times.1)
+    })
+      .max_by(|(_, _, a), (_, _, b)| a.cmp(&b))
+      .unwrap_or(("", 0, 0))
   }
 }
 
@@ -141,7 +150,7 @@ fn parse_line(input: &str) -> Entry {
   let date = date_time_and_action[0]
     .trim_start_matches('[')
     .split(|c| c == '-' || c == ' ' || c == ':')
-    .map(|el| el.parse::<u32>().unwrap_or(999999))
+    .map(|el| el.parse::<u32>().unwrap_or_else(|_| panic!("Number cast error")))
     .collect::<Vec<u32>>();
 
   Entry::new(date[0] as i32,
@@ -175,13 +184,16 @@ fn main() {
   let mut sleep_calculator = SleepTime::new();
   sleep_calculator.process_sleep_times(&parsed_lines);
   let sleepiest_and_sleeps = sleep_calculator.get_sleepiest_elf();
-  let most_common_minute = sleep_calculator.most_common_minute_to_sleep(sleepiest_and_sleeps.0, &parsed_lines);
+  let (most_common_minute, _) = sleep_calculator.most_common_minute_to_sleep(sleepiest_and_sleeps.0, &parsed_lines);
 
   println!("Part one: {} & {} => {}", sleepiest_and_sleeps.0, most_common_minute, multiply_id_with_minute(sleepiest_and_sleeps.0, most_common_minute));
+
+  let res = sleep_calculator.most_minute_to_sleep_by_one_elf(&parsed_lines);
+  println!("Part two: {} & {} => {}", res.0, res.1, multiply_id_with_minute(res.0, res.1));
 }
 
 #[test]
-fn basic_test() {
+fn first_test() {
   let lines = vec!["[1518-11-01 00:00] Guard #10 begins shift",
                    "[1518-11-01 00:05] falls asleep",
                    "[1518-11-01 00:25] wakes up",
@@ -217,5 +229,42 @@ fn basic_test() {
   assert_eq!(sleepiest.0, "#10");
   assert_eq!(sleepiest.1, 50);
   let times = sleep_calculator.most_common_minute_to_sleep("#10", &parsed_lines);
-  assert_eq!(24, times);
+  assert_eq!(times.0, 24);
+}
+
+#[test]
+fn second_test() {
+  let lines = vec!["[1518-11-01 00:00] Guard #10 begins shift",
+                   "[1518-11-01 00:05] falls asleep",
+                   "[1518-11-01 00:25] wakes up",
+                   "[1518-11-01 00:30] falls asleep",
+                   "[1518-11-01 00:55] wakes up",
+                   "[1518-11-01 23:58] Guard #99 begins shift",
+                   "[1518-11-02 00:40] falls asleep",
+                   "[1518-11-02 00:50] wakes up",
+                   "[1518-11-03 00:05] Guard #10 begins shift",
+                   "[1518-11-03 00:24] falls asleep",
+                   "[1518-11-03 00:29] wakes up",
+                   "[1518-11-04 00:02] Guard #99 begins shift",
+                   "[1518-11-04 00:36] falls asleep",
+                   "[1518-11-04 00:46] wakes up",
+                   "[1518-11-05 00:03] Guard #99 begins shift",
+                   "[1518-11-05 00:45] falls asleep",
+                   "[1518-11-05 00:55] wakes up"];
+
+  let mut parsed_lines = lines.iter()
+    .map(|&el| parse_line(el))
+    .collect::<Vec<Entry>>();
+  parsed_lines.sort_by(|a, b| {
+    let duration_a = a.date_time.timestamp();
+    let duration_b = b.date_time.timestamp();
+    duration_a.cmp(&duration_b)
+  });
+
+  let mut sleep_calculator = SleepTime::new();
+  sleep_calculator.process_sleep_times(&parsed_lines);
+
+  let res = sleep_calculator.most_minute_to_sleep_by_one_elf(&parsed_lines);
+  assert_eq!(res.0, "#99");
+  assert_eq!(res.1, 45);
 }
